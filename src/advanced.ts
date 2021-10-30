f45a26e6f2969b8c71037733c6136ab1218156ab
Lin Masahiro <k80092@gmail.com>
2021-10-30 16:24:55 +0900

    add customize message example and publish v1.0.8

--- src/advanced.ts
+++ src/advanced.ts
@@ -2,7 +2,9 @@ import { createApp } from 'vue'
 import CustomizeDisplayData from './examples/CustomizeDisplayData.vue'
 import Filter from './examples/Filter.vue'
 import CustomizeStyle from './examples/CustomizeStyle.vue'
+import CustomizeMessage from './examples/CustomizeMessage.vue'
 
 createApp(CustomizeDisplayData).mount('#customize-display-table')
 createApp(Filter).mount('#filter-table')
 createApp(CustomizeStyle).mount('#customize-style-table')
+createApp(CustomizeMessage).mount('#customize-message-table')
---
+++ src/examples/CustomizeMessage.vue
@@ -0,0 +1,118 @@
+<template>
+  <table-lite
+    :is-loading="table.isLoading"
+    :columns="table.columns"
+    :rows="table.rows"
+    :total="table.totalRecordCount"
+    :sortable="table.sortable"
+    :messages="table.messages"
+    @do-search="doSearch"
+    @is-finished="table.isLoading = false"
+  ></table-lite>
+</template>
+
+<script>
+import { defineComponent, reactive } from "vue";
+import TableLite from "../components/TableLite.vue";
+
+// Fake Data for 'asc' sortable
+const sampleData1 = (offst, limit) => {
+  offst = offst + 1;
+  let data = [];
+  for (let i = offst; i <= limit; i++) {
+    data.push({
+      id: i,
+      name: "TEST" + i,
+      email: "test" + i + "@example.com",
+    });
+  }
+  return data;
+};
+
+// Fake Data for 'desc' sortable
+const sampleData2 = (offst, limit) => {
+  let data = [];
+  for (let i = limit; i > offst; i--) {
+    data.push({
+      id: i,
+      name: "TEST" + i,
+      email: "test" + i + "@example.com",
+    });
+  }
+  return data;
+};
+
+export default defineComponent({
+  name: "App",
+  components: { TableLite },
+  setup() {
+    // Table config
+    const table = reactive({
+      isLoading: false,
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          width: "3%",
+          sortable: true,
+          isKey: true,
+        },
+        {
+          label: "使用者名稱",
+          field: "name",
+          width: "10%",
+          sortable: true,
+        },
+        {
+          label: "電子信箱",
+          field: "email",
+          width: "15%",
+          sortable: true,
+        },
+      ],
+      rows: [],
+      totalRecordCount: 0,
+      sortable: {
+        order: "id",
+        sort: "asc",
+      },
+      messages: {
+        pagingInfo: "現在顯示 {0} 到 {1}筆 共{2}筆",
+        pageSizeChangeLabel: "每頁筆數:",
+        gotoPageLabel: "現在頁數:",
+        noDataAvailable: "沒有資料",
+      },
+    });
+
+    /**
+     * Search Event
+     */
+    const doSearch = (offset, limit, order, sort) => {
+      table.isLoading = true;
+      setTimeout(() => {
+        table.isReSearch = offset == undefined ? true : false;
+        if (offset >= 10 || limit >= 20) {
+          limit = 20;
+        }
+        if (sort == "asc") {
+          table.rows = sampleData1(offset, limit);
+        } else {
+          table.rows = sampleData2(offset, limit);
+        }
+        table.totalRecordCount = 20;
+        table.sortable.order = order;
+        table.sortable.sort = sort;
+      }, 600);
+    };
+
+    // First get data
+    doSearch(0, 10, 'id', 'asc');
+
+    return {
+      table,
+      doSearch,
+    };
+  },
+});
+</script>
+
